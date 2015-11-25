191c7b2c81bfb1adc1ca5e7c001b4d5b9bc29a79
sebastian.castano <sebastian.castano@globant.com>
2015-11-25 08:57:13 -0500

    Conexion externa

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,9 +1,9 @@
 <?php
 //echo "<pre>"; print_r($_SERVER); echo "</pre>";
 
-define('_DB_SERVER_', '181.143.37.190');
+define('_DB_SERVER_', '181.143.37.186');
 define('_DB_NAME_', 'prestashop_matisses16014');
-define('_DB_USER_', 'prestauser16014');
+define('_DB_USER_', 'prestauser16014'); 
 define('_DB_PASSWD_', 'M4T2015&&!10');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
@@ -11,7 +11,7 @@ define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
 define('_PS_CACHE_ENABLED_', '0');
 define('_COOKIE_KEY_', 'Ci0fu32BOM5iT0Vdts0T87Jd9bGE79pOKhIg451XN53ZO7gsEkSQAV1r');
 define('_COOKIE_IV_', 'f1xaKqeO');
-define('_PS_CREATION_DATE_', '2015-06-22');
+define('_PS_CREATION_DATE_', '2015-06-22'); 
 if (!defined('_PS_VERSION_'))
 	define('_PS_VERSION_', '1.6.0.14');
 define('_RIJNDAEL_KEY_', 'nM6wqH5QdaBtqTzBg5xzLNdvndd3XYRf');
