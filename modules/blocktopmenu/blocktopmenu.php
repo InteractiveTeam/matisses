4987db59df0a828325893bf86d165e64366549bd
sebastian.castano <sebastian.castano@globant.com>
2016-02-02 09:57:11 -0500

    label megamenu

--- modules/blocktopmenu/blocktopmenu.php
+++ modules/blocktopmenu/blocktopmenu.php
@@ -647,6 +647,7 @@ class Blocktopmenu extends Module
 				 }
 			if (isset($category['children']) && !empty($category['children']))
 			{
+				$html .= '<div class="label-'.$category['level_depth'].'">';
 				$html .= '<ul>';
 				$html .= $this->generateCategoriesMenu($category['children'], 1);
 
@@ -669,6 +670,7 @@ class Blocktopmenu extends Module
 				}
 
 				$html .= '</ul>';
+				$html .= '</div>';
 			}
 
 			$html .= '</li>';
