a80e872372aa3612c73780f8e2ccc23e4f7879f7
miguelrock22 <miguelrock222@gmail.com>
2016-02-23 17:11:02 -0500

    Cron adelantedo / fix añadir a carrito desde lista

--- modules/giftlist/cron_giftlist.php
+++ modules/giftlist/cron_giftlist.php
@@ -1,20 +1,20 @@
 <?php
 require_once _PS_ROOT_DIR_.'config/config.inc.php';
 require_once 'classes/GiftList.php';
-
-$l = new GiftListModel();
-$lists = $l->getAllLists();
-
+$data = array();
 $paidOrders = $l->getPaidStatusOrder();
 foreach($paidOrders as $order){
     $paidOrder = $l->getPaidsOrders($order['id_order_state']);
     foreach($paidOrder as $row){
-        $prod = $l->getProductsInCartByOrder($row['id_order']);
-        
+        $ids = $l->getProductsInCartByOrder($row['id_order']);
+        foreach($ids as $id_array){
+            $l = new GiftListModel($prod['id_giftlist']);
+            $customer = new CustomerCore($l->id_customer);
+            $product = new ProductCore($id_array['id_product']);
+            $data[] = [
+                'username' => }$customer->fisrtname . " " . $customer->lastname,
+                ''
+            ];
+        }
     }
-}
-
-foreach($lists as $list){
-    $products = $l->getCartProductsByList($list['id']);
-    
 }
