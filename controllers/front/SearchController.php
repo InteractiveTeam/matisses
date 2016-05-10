fab8243c6e4f39ab9b729ac940b0156659a3eddb
Davin anaya <davinson.anaya@arkix.com>
2016-05-10 11:47:25 -0500

    test buscador

--- controllers/front/SearchController.php
+++ controllers/front/SearchController.php
@@ -104,9 +104,13 @@ class SearchControllerCore extends FrontController
 			$this->productSort();
 			$this->n = abs((int)(Tools::getValue('n', Configuration::get('PS_PRODUCTS_PER_PAGE'))));
 			$this->p = abs((int)(Tools::getValue('p', 1)));
+
 			$original_query = $query;
 			$query = Tools::replaceAccentedChars(urldecode($query));
 			$search = Search::find($this->context->language->id, $query, $this->p, $this->n, $this->orderBy, $this->orderWay);
+            
+            echo '<div style="display:none"><pre>'; print_r($search); echo '</pre></div>';
+            
 			if (is_array($search['result']))
 				foreach ($search['result'] as &$product)
 					$product['link'] .= (strpos($product['link'], '?') === false ? '?' : '&').'search_query='.urlencode($query).'&results='.(int)$search['total'];
