be886d7ff7cfb76ccc367d2e1af712556f8a7515
miguelrock22 <miguelrock222@gmail.com>
2016-05-13 09:05:52 -0500

    active debug

--- modules/matisses/createInvoice.php
+++ modules/matisses/createInvoice.php
@@ -1,6 +1,8 @@
 <?php
 include_once('../../config/config.inc.php');
 require_once(dirname(__FILE__).'/matisses.php');
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 $matisses = new matisses();
 echo $matisses->hookactionValidateProductsAvailableCart();
 exit;
