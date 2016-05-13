4d52d3b174e3391604166fbd116ff76df9e29602
miguelrock22 <miguelrock222@gmail.com>
2016-05-13 07:51:54 -0500

    disable debug

--- modules/matisses/createInvoice.php
+++ modules/matisses/createInvoice.php
@@ -1,7 +1,5 @@
 <?php
 include_once('../../config/config.inc.php');
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 require_once(dirname(__FILE__).'/matisses.php');
 $matisses = new matisses();
 echo $matisses->hookactionValidateProductsAvailableCart();
