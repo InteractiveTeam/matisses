364748c0cafd4374dad2616145de471e8d5a23e0
miguelrock22 <miguelrock222@gmail.com>
2016-05-20 18:26:46 -0500

    erroresz

--- modules/matisses/matisses.php
+++ modules/matisses/matisses.php
@@ -1781,9 +1781,7 @@ class matisses extends Module
 												WHERE id_cart = '.$this->context->cookie->id_cart.' 	
 												');	
 
-        echo "lego1".print_r($Addresses);
 		$this->hookactionCustomerAccountUpdate(array('email'=>$this->context->cookie->email),true, $Addresses,$this->context->cookie->id_customer);
-        echo "llego 2";
 	
 		$orderDTO = array();
 		$orderDTO['orderDTO']['header']['prestashopOrderId']= $this->context->cookie->id_cart;
@@ -1797,7 +1795,7 @@ class matisses extends Module
 												WHERE customer_id=".$this->context->customer->id.
 												" AND id_shop = ". $this->context->customer->id_shop.
 												" AND cart_id = ".$this->context->cookie->id_cart);
-        print_r($response);
+        echo "hoñ".print_r($response);
 		if(!$response)
 		{
 			Db::getInstance()->insert('wsmatisses_pagos', array(
