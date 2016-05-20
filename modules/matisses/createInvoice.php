e230c74830d9efaf0f8dd989abf3f2ac6b4b0ded
miguelrock22 <miguelrock222@gmail.com>
2016-05-20 17:27:42 -0500

    cambio prueba

--- modules/matisses/createInvoice.php
+++ modules/matisses/createInvoice.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 1); 
 include_once('../../config/config.inc.php');
 require_once(dirname(__FILE__).'/matisses.php');
 $matisses = new matisses();
