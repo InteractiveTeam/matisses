59105501b6b5f980f5643556caf07cd5b0437d33
Davin anaya <davinson.anaya@arkix.com>
2016-05-28 21:56:04 -0500

    Test carga manual

--- modules/matisses/productos.php
+++ modules/matisses/productos.php
@@ -36,8 +36,7 @@
     
     echo "Davinn => Modelos";
     print_r($_Modelos);
-    exit();
-
+    
 	$_References    = __getReferences($_Modelos);	 
 
 	if(Configuration::get('ax_simpleproduct_data')=='')	{
@@ -677,7 +676,6 @@
 	/* Obetenes las referencias por el modelo
     * $models => todos los modelos delimitados por ","
     */
-
     function getProductsByModel($models){
         $models = array_map(function($el) { return "'{$el}'";},explode(',',$models));
         
