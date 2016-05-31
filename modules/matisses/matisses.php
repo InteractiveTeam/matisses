30a75a5ffdc01598011e66793a9dc97af02afa75
miguelrock22 <miguelrock222@gmail.com>
2016-05-31 17:17:23 -0500

    evitar en confirmaion

--- modules/matisses/matisses.php
+++ modules/matisses/matisses.php
@@ -438,12 +438,14 @@ class matisses extends Module
 	
 	public function hookactionCalculateShipping($params)
 	{
+        $this->page_name = Dispatcher::getInstance()->getController();
         $cart = $this->context->cart;
         $cant_prod = count($cart->getProducts());
         $cache = Cache::retrieve('cart_'.$cart->id);
         if(!empty($cache) && $cache['cart_products'] == $cant_prod && $cache['id_address'] == $params['delivery_option'])
             return json_encode($cache);
-
+        if($this->page_name == 'orderconfirmation')
+            return null;
         $id_address = $params['delivery_option'];
         $id_carrier = str_replace(',','',current(array_values($params['delivery_option'])));
         $shipping_cost = array();
