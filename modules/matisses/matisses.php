df4a7c4dc95cb8f3960e47ae2136378290f53e60
miguelrock22 <miguelrock222@gmail.com>
2016-06-01 00:18:51 -0500

    quitar log

--- modules/matisses/matisses.php
+++ modules/matisses/matisses.php
@@ -469,9 +469,6 @@ class matisses extends Module
                 $salesWarehouseDTO['salesWarehouseDTO']['items'][$k]['quantity'] = $product['quantity'];
             }
             $salesWarehouseDTO = $this->array_to_xml($salesWarehouseDTO,false);
-                    $fp = fopen('log_pagos.txt','w');
-                    fwrite($fp, $salesWarehouseDTO);
-                    fclose($fp);
             $response 	= $this->wsmatisses_get_data('inventoryItem','quoteShipping','pruebas',$salesWarehouseDTO,true);
             if($response['return']['code']=='0101002')
                 $shipping_cost = $this->xml_to_array($response['return']['detail']);
