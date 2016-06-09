dd7dff77d2baf41a8fc3804efc6dca6068a6833a
miguelrock22 <miguelrock222@gmail.com>
2016-06-09 16:35:10 -0500

    errores en sonda

--- modules/matisses/productos.php
+++ modules/matisses/productos.php
@@ -5,6 +5,10 @@
 		@description:	Sonda para la carga de productos sap - prestashop
 			
 	*/  
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
 	session_start();
 	mb_internal_encoding("UTF-8");
 	set_time_limit(0);
