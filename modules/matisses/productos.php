a816761cad815a33e6648465c253b478868b160d
miguelrock22 <miguelrock222@gmail.com>
2016-06-09 21:20:01 -0500

    quitar debug

--- modules/matisses/productos.php
+++ modules/matisses/productos.php
@@ -4,10 +4,7 @@
 		@date: 			mar-2015
 		@description:	Sonda para la carga de productos sap - prestashop
 			
-	*/  
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+	*/
 
 	session_start();
 	mb_internal_encoding("UTF-8");
