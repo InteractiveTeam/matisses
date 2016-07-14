550c1317ecb8d3b1bed502824afcc184100a2bec
sebasrl95 <sebas_94.5@hotmail.com>
2016-07-14 08:08:30 -0500

    Test shop in order

--- modules/matisses/updateshopcode.php
+++ modules/matisses/updateshopcode.php
@@ -6,6 +6,8 @@ $code = Tools::getValue('code');
 $idcart = Tools::getValue('idcart');
 
 if (!empty($code) && !empty($idcart)) {
+    print_r($code);
+    print_r($idcart);
     $matisses = new matisses();
     $result = $matisses->updateShopCode($code, $idcart);
     print_r($result);   
