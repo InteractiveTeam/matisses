82660e2d8a0a5746f43c9abe0f27bf67b048e2b7
sebasrl95 <sebas_94.5@hotmail.com>
2016-07-15 16:27:54 -0500

    Debug recoger en tienda

--- modules/matisses/matisses.php
+++ modules/matisses/matisses.php
@@ -2053,8 +2053,7 @@ class matisses extends Module
 		$orderDTO['orderDTO']['header']['customerId']		= $this->context->customer->charter;
 		$orderDTO['orderDTO']['header']['comments']		= Db::getInstance()->getValue('SELECT message FROM '._DB_PREFIX_."message WHERE id_cart = ".$this->context->cookie->id_cart);
 		$orderDTO['orderDTO']['header']['pickUpStore']	= $codeShop[0]['cod_shop_matisses'];
-        print_r($codeShop[0]['cod_shop_matisses']);
-        print_r($this->context->cookie->id_cart);
+        
 		foreach($products as $d => $v)
 		{
 			$orderDTO['orderDTO']['detail'][$d]['itemCode'] = $products[$d]['reference'];
