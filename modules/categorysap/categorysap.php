5128490f3e84fc817db301a210b568491a9ad3b5
sebasrl95 <sebas_94.5@hotmail.com>
2016-07-21 09:35:41 -0500

    Category SAP Module

--- modules/categorysap/categorysap.php
+++ modules/categorysap/categorysap.php
@@ -66,7 +66,7 @@ class categorysap extends Module
             if (isset($codes)) {
                 
                 foreach ($codes as $key => $code) {                    
-                    $update = Db::getInstance()->ExecuteS('UPDATE '. _DB_PREFIX_ .'category SET subgrupo = "'.$code.'" WHERE id_category = "'.$key.'"'); 
+                    $update = Db::getInstance()->update('category', array('subgrupo' => $code), 'id_category = '.$key);
 
                     if (!$update) {
                         $process = false;
