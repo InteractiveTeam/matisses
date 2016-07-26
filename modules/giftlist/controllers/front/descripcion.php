23b15d1bc76225ae54f1562f5e85644a7891faff
miguelrock22 <miguelrock222@gmail.com>
2016-07-26 11:25:20 -0500

    incidencias

--- modules/giftlist/controllers/front/descripcion.php
+++ modules/giftlist/controllers/front/descripcion.php
@@ -130,8 +130,10 @@ class giftlistdescripcionModuleFrontController extends ModuleFrontController {
                 'msg' => "No hay suficiente producto en inventario"
             )));
         }else{
+            $prod = Db::getInstance()->getValue("SELECT missing FROM "._DB_PREFIX_."list_product_bond WHERE id_product = ".$id_prod . " AND id_list = ".$id_list);
             Db::getInstance()->update('list_product_bond',array(
-                'cant' => $cant
+                'cant' => $cant,
+                'missing' => $cant - (int)$prod
             ),"id_product = ".$id_prod . " AND id_list = ".$id_list);
             die(Tools::jsonEncode(array(
                 'error' => false,
