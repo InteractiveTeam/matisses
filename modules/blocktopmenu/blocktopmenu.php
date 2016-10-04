58c098430480932b27a7cb58631cbaf958d3cee8
davinanaya <davinson.anaya@arkix.com>
2016-10-04 11:33:14 -0500

    menu

--- modules/blocktopmenu/blocktopmenu.php
+++ modules/blocktopmenu/blocktopmenu.php
@@ -518,9 +518,7 @@ class Blocktopmenu extends Module
 		}
 		return $html;
 	}
-    public function isMobile() {
-        return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
-    }
+    
 	private function generateCategoriesMenu($categories, $is_children = 0)
 	{
 		$html = '';
