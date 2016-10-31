c1564d1e07cb8e98715091ab6ca8a2b16b9d61b8
miguelrock22 <miguelrock222@gmail.com>
2016-10-31 10:53:44 -0500

    correción

--- modules/matisses/CargarProductos.php
+++ modules/matisses/CargarProductos.php
@@ -603,7 +603,7 @@ class CargaProductos{
                 $id_prod = Db::getInstance()->getRow("SELECT a.id_product,COUNT(*) as images FROM "._DB_PREFIX_."product a INNER JOIN ps_image b ON a.id_product = b.id_product WHERE a.reference = '".$product->referencia."'");
                 if($id_prod){
                     if((int)$product->activo){
-                        if($id_prod['image'] > 0)
+                        if($id_prod['images'] > 0)
                             $active .= $id_prod.",";
                         else
                             $inactive .= $id_prod.",";
