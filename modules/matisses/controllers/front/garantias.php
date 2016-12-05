bad00d63f816aade498b70627198655f1d4bff84
miguelrock22 <miguelrock222@gmail.com>
2016-12-05 17:39:46 -0500

    std

--- modules/matisses/controllers/front/garantias.php
+++ modules/matisses/controllers/front/garantias.php
@@ -311,7 +311,7 @@ class matissesgarantiasModuleFrontController extends ModuleFrontController
                             
 							$response = Hook::exec('actionAddGarantia', $params );
                             $response = Tools::jsonDecode($response);
-                            if((int)substr($response['return']['code'], 4 , 1)  === 9){
+                            if((int)substr($response->return->code, 4 , 1)  === 9){
                                 $this->errors[] = Tools::displayError($response['return']['detail']);
                             }
                             echo "<pre>"; var_dump($response); echo "</pre>";
